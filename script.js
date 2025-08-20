de872971cafc2df4594ad0b3e3b0d78cf22583ac
Muhammad Farooq <fm6182218@gmail.com>
2025-08-20 23:44:36 +0500

    Portfolio website

---
+++ script.js
@@ -0,0 +1,39 @@
+const mobIcon = document.querySelector(".fa-bars");
+const menuBar = document.getElementById("menu");
+const forms = document.getElementById("form");
+const formsName = document.getElementById("formName");
+console.log(forms);
+
+
+// Menu Bar open and close
+let menu = 0
+mobIcon.addEventListener('click', () => {
+    if (menu === 0) {
+        menuBar.style.display = "block"
+        menu = 1
+    } else {
+        menuBar.style.display = "none"
+        menu = 0
+    }
+})
+// =================
+var typed = new Typed('#typing', {
+    strings: ["Frontend Developer", "HTML", "CSS", "Bootstrap 5", "Tailwind CSS", "JavaScript"],
+    typeSpeed: 100,
+    loop: true,
+    backDelay: 1000,
+})
+// ===================
+
+
+forms.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const input = formsName.value.trim();
+    if (input === "") {
+        alert(`please Enter Your Name`)
+    }
+    else {
+        alert(`WellCome to My Portfoilo WebSite "${input}"`)
+        forms.reset().
+    }
+})
